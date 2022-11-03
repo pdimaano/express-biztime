dd0e27773ddf19b3d7c8de073b6485b98bffd9b3
trevorhudson <trevor.c.hudson@gmail.com>
2022-11-03 14:41:17 -0700

    added new ErrorHandler

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const express = require("express");
 const app = express();
 const compRoute = require("./routes/companies");
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 
 app.use(express.json());
